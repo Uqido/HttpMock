aa44f8a53e1c03e46c9eb03205784437ee1f6178
Antonio Ruggiero <antonio.ruggiero@uqido.com>
2021-11-08 14:37:34 +0100

    fixed not passing tests, updated package.json

--- Tests/Editor/Integration Tests/EndpointsReturningFilesTests.cs
+++ Tests/Editor/Integration Tests/EndpointsReturningFilesTests.cs
@@ -2,35 +2,35 @@ using System;
 using System.IO;
 using System.Net;
 using NUnit.Framework;
+using UnityEngine;
 
 namespace HttpMock.Integration.Tests
 {
 	[TestFixture]
-	public class EndpointsReturningFilesTests	
+	public class EndpointsReturningFilesTests
 	{
-		private const string FILE_NAME = "transcode-input.mp3";
-		private static readonly string RES_TRANSCODE_INPUT_MP3 = Path.Combine("res",FILE_NAME);
+		private const string FILE_PATH = "Packages/com.uqido.httpmock/Tests/Resources/MyTestFile.txt";
 
 		[Test]
 		public void A_Setting_return_file_return_the_correct_content_length() {
 			var stubHttp = HttpMockRepository.At("http://localhost.:9191");
 
+			var file = Path.GetFullPath(FILE_PATH);
 
-		    var pathToFile = Path.Combine(TestContext.CurrentContext.TestDirectory, RES_TRANSCODE_INPUT_MP3);
-
-		    stubHttp.Stub(x => x.Get("/afile"))
-				.ReturnFile(pathToFile)
+			UnityEngine.Debug.Log(file);
+			stubHttp.Stub(x => x.Get("/afile"))
+				.ReturnFile(file)
 				.OK();
 
 			Console.WriteLine(stubHttp.WhatDoIHave());
             
-			var fileLength = new FileInfo(pathToFile).Length;
+			var fileLength = new FileInfo(file).Length;
 
 			var webRequest = (HttpWebRequest) WebRequest.Create("http://localhost.:9191/afile");
 			using (var response = webRequest.GetResponse())
 			using(var responseStream = response.GetResponseStream())
 			{
-
+			
 				var bytes = new byte[response.ContentLength];
 				responseStream.Read(bytes, 0, (int) response.ContentLength);
 				
